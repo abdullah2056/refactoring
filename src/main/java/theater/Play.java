48ac19a4b5238f4ac48a6aa8f70338719b85ab98
abdullah2056 <abdullah_2056@hotmail.com>
2025-11-24 09:27:09 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,25 @@
 package theater;
 
+/**
+ * Represents a play with a name and type.
+ *
+ * @null This class requires no additional Javadoc tags.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
